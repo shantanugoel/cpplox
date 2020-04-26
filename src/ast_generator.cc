1ced4940245e22d6851627faeb1652f27b9b4ae4
Shantanu Goel <shantanu.goel@gmail.com>
2020-04-26 19:53:20 +0530

    output AST to stdout instead of file. Add genrule to generate AST during build

--- src/ast_generator.cc
+++ src/ast_generator.cc
@@ -1,6 +1,4 @@
 #include <algorithm>
-#include <filesystem>
-#include <fstream>
 #include <iostream>
 #include <sstream>
 #include <string>
@@ -22,11 +20,11 @@ std::string rtrim(const std::string& s) {
 
 std::string trim(const std::string& s) { return rtrim(ltrim(s)); }
 
-void DefineType(std::ofstream& of, const std::string& base_name,
+void DefineType(std::ostream& os, const std::string& base_name,
                 const std::string& class_name, const std::string& fields) {
-  of << "\nclass " << class_name << " : public " << base_name << " {\n";
-  of << " public:\n";
-  of << "  " << class_name << " (" << fields << ")"
+  os << "\nclass " << class_name << " : public " << base_name << " {\n";
+  os << " public:\n";
+  os << "  " << class_name << " (" << fields << ")"
      << " : ";
   int delimiter_loc_prev = 0;
   int delimiter_loc = -1;
@@ -39,74 +37,52 @@ void DefineType(std::ofstream& of, const std::string& base_name,
     const std::string field_type = trim(temp.substr(0, temp.find(" ")));
     const std::string field_name = trim(temp.substr(temp.find(" ")));
     if (delimiter_loc_prev != 0) {
-      of << ", ";
+      os << ", ";
     }
-    of << field_name << "(" << field_name << ")";
+    os << field_name << "(" << field_name << ")";
     private_members << "  " << field_type << " " << field_name << ";\n";
   } while (delimiter_loc != -1);
 
-  of << " {}\n";
-  of << "\n private:\n";
-  of << private_members.str();
-  of << "};\n";
+  os << " {}\n";
+  os << "\n private:\n";
+  os << private_members.str();
+  os << "};\n";
 }
 
-void DefineAst(std::string_view output_dir, const std::string& base_name,
+void DefineAst(const std::string& base_name,
                const std::vector<const std::string> types) {
-  const std::string file_name = base_name + ".h";
-  fs::path output_file_path(output_dir);
+  std::stringstream os;
+  std::string file_header_temp;
+  file_header_temp.resize(base_name.length());
+  std::transform(base_name.begin(), base_name.end(), file_header_temp.begin(),
+                 ::toupper);
+  os << "// Auto Generated file. Do not edit by hand.\n";
+  os << "#ifndef _" << file_header_temp << "_H_\n";
+  os << "#define _" << file_header_temp << "_H_\n";
+  os << "namespace lox {\n";
+  os << "#include \"token.h\"\n";
+  os << "class " << base_name << " {\n";
+  os << "};\n";
 
-  std::error_code ec;
-  ec.clear();
-  fs::create_directory(output_file_path, ec);
-  if (ec) {
-    std::cerr << ec.message();
-    return;
+  for (auto& type : types) {
+    size_t delimiter_location = type.find(":");
+    std::string class_name = trim(type.substr(0, delimiter_location));
+    std::string fields = trim(type.substr(delimiter_location + 1));
+    DefineType(os, base_name, class_name, fields);
   }
 
-  output_file_path /= file_name;
-  std::ofstream of(output_file_path, std::ios::out);
-  if (!of.is_open()) {
-    std::cerr << std::strerror(errno);
-  } else {
-    std::string file_header_temp;
-    file_header_temp.resize(base_name.length());
-    std::transform(base_name.begin(), base_name.end(), file_header_temp.begin(),
-                   ::toupper);
-    of << "// Auto Generated file. Do not edit by hand.\n";
-    of << "#ifndef _" << file_header_temp << "_H_\n";
-    of << "#define _" << file_header_temp << "_H_\n";
-    of << "namespace lox {\n";
-    of << "#include \"token.h\"\n";
-    of << "class " << base_name << " {\n";
-    of << "};\n";
-
-    for (auto& type : types) {
-      size_t delimiter_location = type.find(":");
-      std::string class_name = trim(type.substr(0, delimiter_location));
-      std::string fields = trim(type.substr(delimiter_location + 1));
-      DefineType(of, base_name, class_name, fields);
-    }
-
-    of << "}  // namespace lox\n";
-    of << "#endif\n";
-    of.close();
-  }
+  os << "}  // namespace lox\n";
+  os << "#endif\n";
+  std::cout << os.str();
   return;
 }
 
-int main(int argc, char** argv) {
-  if (argc != 2) {
-    std::cerr << "Usage: generate_ast <output directory>\n";
-    return 1;
-  }
-
-  std::string output_dir = argv[1];
+int main() {
   std::vector<const std::string> types = {
       "Binary: Expr left, Token operator, Expr right",
       "Grouping: Expr expression", "Literal: Object value",
       "Unary: Token opreator, Expr right"};
 
-  DefineAst(output_dir, "Expr", types);
+  DefineAst("Expr", types);
   return 0;
 }
